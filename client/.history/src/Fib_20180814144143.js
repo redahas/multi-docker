0863c3a6381073d4a36fadb3f3807d02ddfb1353
redahas <seanredahan@gmail.com>
2019-01-13 00:56:26 +0000

    If not object returned by axios

--- client/.history/src/Fib_20180814144143.js
+++ client/.history/src/Fib_20180814144143.js
@@ -20,12 +20,9 @@ class Fib extends Component {
 
   async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
-
-    if (seenIndexes && seenIndexes.data) {
-      this.setState({
-        seenIndexes: seenIndexes.data
-      }); 
-    }
+    this.setState({
+      seenIndexes: seenIndexes.data
+    });
   }
 
   handleSubmit = async event => {
